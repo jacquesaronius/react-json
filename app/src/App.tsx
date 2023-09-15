910227056190840d63ef0e232ba8e2323789884a
Jacques Bourquin <jacques@jacoscript.com>
2023-09-15 18:36:21 -0500

    Working on dynamic updates

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,15 +1,43 @@
-import React from 'react';
-import { Container, Typography, CircularProgress } from "@mui/material";
+import React, { ReactElement, useEffect, useRef } from 'react';
+import { Container, Typography, CircularProgress, Table, TableBody, TableRow, TableCell } from "@mui/material";
 import logo from './logo.svg';
 import './App.css';
+import { OverridableComponent } from '@mui/material/OverridableComponent';
 
 function App() {
+  const progress = useRef<HTMLElement>(null);
+  const systems = useRef<HTMLTableSectionElement>(null);
+  
+  useEffect(() => {
+    //read json file 
+    fetch('../data.json').then(res => res.json()).then(data => {
+      //set progress bar to 100%
+      if (progress.current != null)
+        progress.current.style.display = 'none';
+      //add data to table
+  
+      data.forEach((system: any) => {
+        if (systems.current != null) {
+          const row = 
+          <TableRow>
+            <TableCell>{system.name}</TableCell>
+          </TableRow>
+          systems.current.appendChild(row as Node)
+          );
+        }
+      });
+
+    });
+  });
   return (
     <div className="App">
       <Container>
         <Typography variant="h4">Systems</Typography>
-        <div id="systems">
-          <CircularProgress />
+        <CircularProgress ref={progress} />
+        <div>
+          <Table>
+            <TableBody ref={systems}/>
+          </Table>
         </div>
       </Container>
     </div>
