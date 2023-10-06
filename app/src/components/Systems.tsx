9f445c066baa3c70b544caee3b7fa881b6b504b0
Jacques Bourquin <jacques@jacoscript.com>
2023-10-05 19:36:32 -0500

    Added some effects

--- app/src/components/Systems.tsx
+++ app/src/components/Systems.tsx
@@ -1,10 +1,24 @@
 import React, { useEffect, useRef } from 'react';
 import { Container, Typography, CircularProgress, Table, TableBody, TableRow, TableCell } from "@mui/material";
-import data from '../../../data.json';
+import data from '../data.json';
 
 function Systems() {
     const progress = useRef<HTMLElement>(null);
     const systems = useRef<HTMLDivElement>(null);
+    const sleep = (ms:number) => new Promise(resolve => setTimeout(resolve, ms));
+    
+    useEffect(() => {
+        async function update() {
+            await sleep(5000);
+    
+            if (progress.current && systems.current) {
+                progress.current.className = "invisible";
+                systems.current.className = "visible";
+            }
+        }
+        update();
+    });
+    
     return (
         <div className="systems">
             <Container>
@@ -26,7 +40,6 @@ function Systems() {
             </Container>
         </div>
     )
-
 }
 
 export default Systems;
